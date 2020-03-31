faec63d85662cf7db77970f6ff50fb5cf0450a34
Ron Martin <ronmartin89@gmail.com>
2020-03-31 12:11:30 -0700

    completed exercise 10

--- what-am-i.js
+++ what-am-i.js
@@ -1,13 +1,17 @@
 // What does the following algorithm do? What is the Big O of the following algorithm? Explain your answer
-
+// This function checks if a number is prime
 function isWhat(n) {
-  if (n < 2 || n % 1 !== 0) {
+  if (n < 2 || n % 1 !== 0) {  //O(1)
     return false;
   }
-  for (let i = 2; i < n; ++i) {
+  for (let i = 2; i < n; ++i) { //O(n) 
+    // since its ++i, if the number was 2, the best case is O(1)
     if (n % i === 0) return false;
   }
-  return true;
+  return true; //O(1)
 }
 
-// INPUT: 
+// INPUT:  7 or 2
+// Worst-case: Linear O(n)
+// Avg-Case: Liner O(n)
+// Best-case: Constant O(1)  If input was 2
